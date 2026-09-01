edebbb80e0bb71e0625f2a24750234915616783a
Xzwka1 <junpongearth@gmail.com>
2026-09-01 09:19:22 +0700

    LCT01

--- Assets/Scripts/Workspace/Assignment/StudentSolution/LCT01SyntaxClass.cs
+++ Assets/Scripts/Workspace/Assignment/StudentSolution/LCT01SyntaxClass.cs
@@ -5,16 +5,34 @@ namespace Assignment.StudentSolution.LCT01
 {
     public class Car
     {
+        public string name;
+        public float speed;
+        public string color;
 
+        public void Move()
+        {
+            Debug.Log("Car is moving");
+        }
+
+        public void Turn()
+        {
+            Debug.Log("Car is turning");
+        }
     }
 
     public class LCT01SyntaxClass
     {
         public void Start()
         {
-            // Student code start HERE ...
+            Car  car1 = new Car();
+            car1.name = "Lambogini";
+            Debug.Log(car1.name);
+            car1.Move();
+            car1.Turn();
 
-            // Student code ends HERE 
+            Car car2 = new Car();
+            car2.name = "Toyota";
+            Debug.Log(car2.name); 
         }
     }
 }
